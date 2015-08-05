436764afd0c0eccf1511427ff23c133281755b2d
Hanneybadger <hwillen@gmail.com>
2015-08-05 11:20:42 +0200

    03
    tried to difine the prices in list. worked fine.

--- trial.py
+++ trial.py
@@ -7,3 +7,8 @@ book_number = 1
 book_vat = 1.06
 
 print robot_price*robot_number*robot_vat+book_price*book_number*book_vat
+
+
+customer_id = [900, 2, 1.25, 100, 1, 1.06]
+
+print customer_id[0]*customer_id[1]*customer_id[2]+customer_id[3]*customer_id[4]*customer_id[5]

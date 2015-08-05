839b63b9d38a2fbc4e0b16634af708b452b9f5bc
Hanneybadger <hwillen@gmail.com>
2015-08-05 11:27:31 +0200

    steg 03
    funkar

--- trial.py
+++ trial.py
@@ -12,3 +12,9 @@ print robot_price*robot_number*robot_vat+book_price*book_number*book_vat
 customer_id = [900, 2, 1.25, 100, 1, 1.06]
 
 print customer_id[0]*customer_id[1]*customer_id[2]+customer_id[3]*customer_id[4]*customer_id[5]
+
+
+robot = {"price": 900, "number": 2, "vat": 1.25}
+book = {"price": 100, "number": 1, "vat": 1.06}
+
+print robot["price"]*robot["number"]*robot["vat"]+book["price"]*book["number"]*book["vat"]

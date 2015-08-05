47c9269b67c86ce48b9239c92a41ff65fd8735b6
Hanneybadger <hwillen@gmail.com>
2015-08-05 11:11:45 +0200

    02

--- trial.py
+++ trial.py
@@ -1,1 +1,9 @@
-print 900*2*1.25+100*1*1.06
+robot_price = 900
+robot_number = 2
+robot_vat = 1.25
+
+book_price = 100
+book_number = 1
+book_vat = 1.06
+
+print robot_price*robot_number*robot_vat+book_price*book_number*book_vat
